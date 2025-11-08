4c2f0957a05b994d3e90acfbee5b9ac13f870e98
faisale <fa6769@gmaile.com>
2025-11-08 19:46:22 +0600

    server side

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+  
+      <h1 className='text-6xl text-center'>ite + React</h1>
+   
+      <p className="text-2xl text-center ">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
